e56f378104ef19c786ed09343b04650870c9edb9
Ali Abbas <alia63581@gmail.com>
2020-10-23 19:00:55 +0500

    Minor changes for Production

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit', (e)=>{
     e.preventDefault();
     
     weatherbox.innerHTML="<div class='weather-box'> Loading ... </div>";
-    fetch('http://localhost:3000/weather?address='+search.value).then((response) =>{
+    fetch('/weather?address='+search.value).then((response) =>{
         
         response.json().then((data)=>{
             if(data.error)
